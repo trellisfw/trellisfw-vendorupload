ee192fc102cc615418dd8e8ebb40610a6c47ff8f
nate <nate.givens03@gmail.com>
2019-11-04 21:48:40 -0500

    fix: put actions directly in sequences because there wasn't much to them yet

--- src/cerebral/sequences.js
+++ src/cerebral/sequences.js
@@ -1,7 +1,40 @@
-import * as actions from './actions';
+// 1.  need to define 
+// 2.  byte range that skips fails
+// 3.  non-skips should pass
+// 4.  header to represent byte range to upload
+// 4.5 loop through byte range and put
+// 4.6 setting for max file size
+
+// - webdriver.io
+
+import { state, props } from 'cerebral';
+import uuid from 'uuid';
 
 export const setFile = [
-  actions.setFile,
-  actions.uploadFile
+  // set file values in state
+  ({ store, get }) => {
+    store.set(state`fileUpload.file_name`, get(props`file_name`));
+    store.set(state`fileUpload.is_loaded`, true);
+    store.set(state`fileUpload.uuid`, uuid.v4());
+    console.log(get(state`fileUpload`));
+  },
+
+  // upload file to server
+  // TODO change to sending file to oada
+  async ({ get, http }) => {
+    try {
+      var formData = new FormData();
+      formData.append('file', document.getElementById('file').files[0]);
+
+      /* const _req = */
+      await http.post('http://localhost:8000', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+    } catch(e) {
+      console.error(e);
+    }
+  }
 ];
 
