557087c62aff513d92ae35e7b5ca9d46c827e125
Nathan Givens <nate.givens03@gmail.com>
2019-12-15 21:50:10 -0500

    share: sharing notes to other computers

--- src/cerebral/state.js
+++ src/cerebral/state.js
@@ -1,3 +1,15 @@
+// TODO:
+// 1. expected documents
+//    - if not present, show drop window for doc
+//    a. insurance form
+//    b. letter of gaurantee
+//    c. location
+//        - audit information
+//
+// 2. expiration date
+//    - if not known ask with red-bordered textbox
+//    - pdfjs
+
 export default {
   title: 'Binary Upload',
   fileUpload: {
